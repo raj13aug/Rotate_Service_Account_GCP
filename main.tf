afa6704a53af080a6396e7e6ae59c748456e7ad0
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-12-06 22:55:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,10 +14,22 @@ resource "google_service_account_key" "demo_sa_key" {
   }
 }
 
-resource "local_file" "sa_json_file" {
-  content  = base64decode(google_service_account_key.demo_sa_key.private_key)
-  filename = "${path.module}/demo_key.json"
-
+resource "google_secret_manager_secret" "demo_secret" {
+  secret_id = "demo_service_account_key_secret"
+  replication {
+    auto {}
+  }
 }
+
+resource "google_secret_manager_secret_version" "key_secret_version" {
+  secret      = google_secret_manager_secret.demo_secret.id
+  secret_data = base64decode(google_service_account_key.demo_sa_key.private_key)
+}
+
+# resource "local_file" "sa_json_file" {
+#   content  = base64decode(google_service_account_key.demo_sa_key.private_key)
+#   filename = "${path.module}/demo_key.json"
+
+# }
 
 # https://cloud.google.com/iam/docs/key-rotation

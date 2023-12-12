1462ee0077c95bee17284788f702015f5e28bf81
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-12-12 22:59:50 +0530

    uuuug

--- main.tf
+++ main.tf
@@ -13,12 +13,3 @@ resource "google_service_account_key" "demo_sa_key" {
     rotation_time = time_rotating.sa_key_rotation.rotation_rfc3339
   }
 }
-
-
-resource "local_file" "sa_json_file" {
-  content  = base64decode(google_service_account_key.demo_sa_key.private_key)
-  filename = "${path.module}/demo_key.json"
-
-}
-
-# https://cloud.google.com/iam/docs/key-rotation
